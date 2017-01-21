db013c8916beac9e0a80054b1b051be7535ec513
Wowo <wojtekwojtek529@gmail.com>
2017-01-22 00:50:02 +0100

    debug couts deleted

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -61,13 +61,6 @@ void GameState::Update(std::chrono::milliseconds delta_time)
 		(DestructionsEnded() || GameModule::input->GetKeyState(SDL_SCANCODE_SPACE))) ||
 		(std::chrono::high_resolution_clock::now() > playertimer))
 	{	
-		if (blockshooting)
-			std::cout << "1\n";
-		if (DestructionsEnded())
-			std::cout << "2\n";
-		if (std::chrono::high_resolution_clock::now() > playertimer)
-			std::cout << "3\n";
-
 		FadeInEffect();
 	}
 
@@ -243,8 +236,6 @@ void GameState::ResetDestructTimer()
 {
 	destruct_timer = std::chrono::high_resolution_clock::now() +
 		std::chrono::seconds(GameModule::resources->GetIntParameter("destructime"));
-
-	std::cout << GameModule::resources->GetIntParameter("destructime") << " KURWA\n";
 }
 
 bool GameState::DestructionsEnded()
